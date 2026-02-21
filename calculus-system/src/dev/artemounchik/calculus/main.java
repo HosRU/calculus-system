635528188272fdf38b147f611e82e3c9e3721538
Tyomk <Tyomka_Anderson@mail.ru>
2026-02-21 22:22:29 +0300

    Revert "Проработка перевода v2"
    This reverts commit 3a75719b172cf282e6bfb66b723d13c33cdeb11e.

--- calculus-system/src/dev/artemounchik/calculus/main.java
+++ calculus-system/src/dev/artemounchik/calculus/main.java
@@ -1,35 +1,10 @@
 package dev.artemounchik.calculus;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class main {
 
 	public static void main(String[] args) {
-		int sourceNumber = 56327;
-		
-		translition(sourceNumber, 9);
-	}
-	
-	private static void translition(int sourceNumber, int system) {
-		List<Integer> arrayNumbersInteger = new ArrayList<Integer>(16);
-		List<Integer> resultArrayNumbersIntegers = new Arraylist<Integer>();
-		
-		String stockViewString = String.valueOf(sourceNumber);
-		System.out.println("Исходное значение: "+ stockViewString);
-		
-		for(int count = 0; count < stockViewString.length(); count++) {
-			String charString = String.valueOf(stockViewString.charAt(count));
-			int number = Integer.parseInt(charString);
-			arrayNumbersInteger.add(number);
-			
-			for(int internalCounter = 0; internalCounter < arrayNumbersInteger.length(); internalCounter++) {
-				
-			}
-		}
+		// TODO Auto-generated method stub
 
-		for(int number : arrayNumbersInteger) {
-			System.out.println("Число: " + number);
-		}
 	}
+
 }
